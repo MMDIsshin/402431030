8cc2a932e1fb99d1dbcb70deb73c9bda18165c95
MMD <161433960+MMDIsshin@users.noreply.github.com>
2024-04-19 17:54:26 +0330

    DAMN rg ...

---
+++ BinaryNum/BinaryNum/BinaryNum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main() {
+	int in; list<int>outl;
+	cout << "Please give me a number : " << endl;
+	cin >> in;
+	while (in != 1) {
+		outl.emplace_front(in % 2);
+		in = in / 2;
+	}
+	outl.emplace_front(1);
+	for (auto i = outl.begin(); i != outl.end(); i++)
+	{
+			cout << *i;
+	}
+	
+}
+// ICE T MODE ;)
+//-MMD.
+
